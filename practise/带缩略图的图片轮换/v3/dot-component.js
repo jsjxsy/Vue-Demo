37d480d73c6bd7eae8e8d60752f8f0d7605dfb5a
xsy <>
2021-04-02 15:44:37 +0800

    添加 新的功能

---
+++ practise/带缩略图的图片轮换/v3/dot-component.js
@@ -0,0 +1,65 @@
+export default {
+    template: `<ul>
+        <li v-for="(item, i) in images" :key="i">
+            <img :src="item" class="tip-image" ref="tip">
+            <img :src="index === i ? dotImages[0]:dotImages[1]"
+                 @click="current(i)" @mouseenter="enter(i)" @mouseleave="leave(i)"/>
+        </li>
+    </ul>`,
+    props: {
+        images: {
+            type: Array,
+            default: function () {
+                return []
+            },
+            required: true
+
+        },
+        currentIndex: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        dotImages: {
+            type: Array,
+            default: function () {
+                return []
+            },
+            required: true
+        }
+    },
+    data() {
+        return {
+            index:this.currentIndex
+        }
+
+    },
+    methods: {
+        current(index) {
+            this.index = index
+            this.$store.commit('change',this.index)
+            //eventBus.$emit('changeCurrentIndex', this.index)
+        },
+        enter(index) {
+            this.$refs.tip[index].style.display = 'block'
+        },
+        leave(index) {
+            this.$refs.tip[index].style.display = 'none'
+        }
+    },
+    watch: {
+        currentIndex: function (oldValue, newValue) {
+            this.$refs.dot[oldValue].src = 'img/normal.png'
+            this.$refs.dot[newValue].src = 'img/active.png'
+        },
+        '$store.state.currentIndex'() {
+            this.index = this.$store.state.currentIndex
+        }
+    },
+    mounted() {
+        // eventBus.$on('changeIndex', function (index) {
+        //     console.debug('changeIndex='+index)
+        //     this.index = index
+        // }.bind(this))
+    }
+}
